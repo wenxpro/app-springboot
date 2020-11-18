1bf4d4f1f2b5a2cbb1db86c5b6b162e91c8ab8b4
长歌行 <wxpro@vip.qq.com>
2020-11-18 16:24:06 +0800

    init commit

---
+++ app-module/src/main/java/com/fivesoft/appmodule/AppModuleApplication.java
@@ -0,0 +1,13 @@
+package com.fivesoft.appmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppModuleApplication.class, args);
+    }
+
+}
