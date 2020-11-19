b0304add72be206d238061f655c5dcfd16d5a18d
长歌行 <wenxpro@outlook.com>
2020-11-19 20:39:34 +0800

    ref

---
+++ app-framework/app-auth/src/main/java/org/fivesoft/config/CacheConfig.java
@@ -0,0 +1,38 @@
+package org.fivesoft.config;
+
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.cache.impl.TimedCache;
+import org.fivesoft.common.cache.UserCache;
+import org.fivesoft.context.constant.ConstantContextHolder;
+import org.fivesoft.login.SysLoginUser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 用户缓存配置
+ * @author wenx
+ * @date 2020-11-19
+ */
+@Configuration
+public class CacheConfig {
+
+    /**
+     * 登录用户的缓存，默认过期时间，根据系统sys_config中的常量决定
+     *
+     * @author stylefeng
+     * @date 2020/7/9 11:44
+     */
+    @Bean
+    public UserCache userCache() {
+        TimedCache<String, SysLoginUser> timedCache =
+                CacheUtil.newTimedCache(ConstantContextHolder.getSessionTokenExpireSec() * 1000);
+
+        //定时清理缓存，间隔1秒
+        timedCache.schedulePrune(1000);
+
+        return new UserCache(timedCache);
+    }
+
+
+}
---
+++ app-framework/app-system/src/main/java/org/fivesoft/config/RestCacheConfig.java
@@ -0,0 +1,50 @@
+package org.fivesoft.config;
+
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.cache.impl.TimedCache;
+import org.fivesoft.common.cache.MappingCache;
+import org.fivesoft.common.cache.ResourceCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * 缓存的配置，默认使用基于内存的缓存，如果分布式部署请更换为redis
+ *
+ * @author xuyuxiang
+ * @date 2020/7/9 11:43
+ */
+@Configuration
+public class RestCacheConfig {
+
+    /**
+     * mapping映射缓存
+     *
+     * @author fengshuonan
+     * @date 2020/7/24 13:55
+     */
+    @Bean
+    public MappingCache mappingCache() {
+        TimedCache<String, Map<String, Object>> timedCache =
+                CacheUtil.newTimedCache(2 * 60 * 1000);
+
+        // 定时清理缓存，间隔1秒
+        timedCache.schedulePrune(1000);
+
+        return new MappingCache(timedCache);
+    }
+
+
+    /**
+     * url资源的缓存，默认不过期
+     *
+     * @author stylefeng
+     * @date 2020/7/9 11:44
+     */
+    @Bean
+    public ResourceCache resourceCache() {
+        return new ResourceCache();
+    }
+
+}
